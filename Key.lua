5e80286e330db1f15b78353adf1258af226a6f09
LormMer <162871368+LormMer@users.noreply.github.com>
2024-03-10 16:10:14 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -103,7 +103,7 @@ TextBox.BorderSizePixel = 0
 TextBox.Position = UDim2.new(0.0184049085, 0, 0.208469048, 0)
 TextBox.Size = UDim2.new(0, 470, 0, 60)
 TextBox.Font = Enum.Font.FredokaOne
-TextBox.Text = text
+TextBox.Text = _G.Key
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.TextSize = 50.000
 
@@ -155,7 +155,9 @@ Rejoin.TextSize = 50.000
 UIGradient_5.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(90, 8, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
 UIGradient_5.Parent = Rejoin
 
-
+spawn(function()
+    task.spawn(_G.Key)
+end)
 
 Connect.MouseButton1Down:connect(function()
     print(_G.Key)
